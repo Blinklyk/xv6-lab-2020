dc9f21074f6ae065eae08a3babe925fa81a01bd6
Blinklyk <290913796@qq.com>
2022-01-16 19:19:26 +0000

    lab1-sleep

---
+++ sleep.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if (argc != 2) {
+    fprintf(2, "sleep failed123\n");
+    exit(1);
+  } else {
+    int duration = atoi(argv[1]);
+    sleep(duration);
+    exit(0);
+  }
+}
